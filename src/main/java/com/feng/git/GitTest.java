1a6b982d824b145f4a894091a5723838068cdd17
sxfstart <59354490+sxfstart@users.noreply.github.com>
2023-06-11 14:40:55 +0800

    pull commit

--- src/main/java/com/feng/git/GitTest.java
+++ src/main/java/com/feng/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git hot-fix 2");
         System.out.println("hello git master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
